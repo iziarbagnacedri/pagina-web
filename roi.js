cb976130cfc673f064957b3fcff5886ecb092e29
iziarbagnacedri <ibagnacedri@itba.edu.ar>
2024-05-02 17:59:58 -0300

    cambios varios

--- roi.js
+++ roi.js
@@ -10,5 +10,3 @@ function calcularROI() {
     document.getElementById("resultadoROI").style.display = "block";
     document.getElementById("roiResultado").innerText = "El ROI de la propiedad es: " + ROI.toFixed(2) + "%";
 }
-
-
