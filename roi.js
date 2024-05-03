559323ddb9539d1e7f1094e7e9ee318453809fd5
iziarbagnacedri <ibagnacedri@itba.edu.ar>
2024-05-03 18:57:09 -0300

    cambios

--- roi.js
+++ roi.js
@@ -10,3 +10,14 @@ function calcularROI() {
     document.getElementById("resultadoROI").style.display = "block";
     document.getElementById("roiResultado").innerText = "El ROI de la propiedad es: " + ROI.toFixed(2) + "%";
 }
+
+
+document.getElementById("formularioContacto").addEventListener("submit", function(event) {
+    event.preventDefault(); // Evita que se envíe el formulario por defecto
+    
+    // Aquí podrías realizar alguna validación del formulario si lo deseas
+    
+    // Simulación de envío del formulario (aquí enviarías los datos al servidor)
+    // Después de enviar exitosamente, muestra el mensaje de éxito
+    document.getElementById("mensajeEnvio").style.display = "block";
+});
